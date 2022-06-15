28ccee51b784859027cf0f645bf4cd66fe76b9be
Annasutha <annasutha.s@lanwaresolutions.com>
2022-06-15 22:34:47 +0530

    first commit

---
+++ lib/view/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:ailoitte/view/response_page.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import '../modelclass/response.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Dio _dio = Dio();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ailotte"),
+      ),
+      body: FutureBuilder<ResponseData>(
+          future: _requestData(),
+          builder: (context,  snapshot) {
+            if (snapshot.hasData) {
+              var screenSize = MediaQuery.of(context).size;
+          return Container(
+            child: Text(snapshot.data.toString()),
+          );
+
+            }
+            return const Center(child: CircularProgressIndicator());
+          })
+    );
+  }
+  Future<ResponseData> _requestData() async {
+    const _baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=rum';
+    ResponseData? responseData;
+    Response response = await _dio.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=rum");
+    print('User Info: ${response.data}');
+    responseData = ResponseData.fromJson(response.data);
+    return ResponseData.fromJson(response.data);
+
+
+}
+  Widget _requestResponseData(){
+    return ElevatedButton(onPressed: (){
+      _requestData();
+      /*Future.delayed(const Duration(seconds: 8));
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const ResponsePage()));*/
+    }, child: const Text("Click Here!"));
+  }
+}
---
+++ lib/view/response_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ResponsePage extends StatefulWidget {
+  const ResponsePage({Key? key}) : super(key: key);
+
+  @override
+  State<ResponsePage> createState() => _ResponsePageState();
+}
+
+class _ResponsePageState extends State<ResponsePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Text(""),
+          ],
+        ),
+      ),
+    );
+  }
+}
